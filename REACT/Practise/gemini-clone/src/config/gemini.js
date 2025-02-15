361de45bbeb71d80cfd0f3c15c36ac4f9ac8ce03
“[Piyush <118821247+Piyush0053@users.noreply.github.com>
2025-02-15 16:07:40 +0530

    Gemini_Clone

--- REACT/Practise/gemini-clone/src/config/gemini.js
+++ REACT/Practise/gemini-clone/src/config/gemini.js
@@ -1,35 +1,33 @@
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from  "@google/generative-ai";
 
+const apiKey ="AIzaSyBN9orIrOmavO1deHSLmEYoSXd_72-Xp_U";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+const genAI = new GoogleGenerativeAI(apiKey);
 
-const {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } = require("@google/generative-ai");
-  
-  const apiKey = process.env.GEMINI_API_KEY;
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash",
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {  
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
   });
-  
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 40,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-  };
-  
-  async function run(prompt) {
-    const chatSession = model.startChat({
-      generationConfig,
-      history: [
-      ],
-    });
-  
-    const result = await chatSession.sendMessage(prompt);
-    console.log(result.response.text());
-  }
-  
-export default run(); 
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return response.text();
+}
+export default run;
--- REACT/Practise/gemini-clone/src/context/context.jsx
+++ REACT/Practise/gemini-clone/src/context/context.jsx
@@ -1,23 +1,57 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from '../config/gemini';
 
 export const Context= createContext();
 
 const ContextProvider =(props)=>{
+    //  input -->use to save the input data
+    const [input,setInput]=useState("");
+    // recentPrompt --> display the prompt we have written
+    const [recentPrompt, setRecentPrompt]=useState("");
+    // prevPrompt--> use to store all the input history and display in the sidebar
+    const [prevPrompt, setPrevPrompt]= useState([]);
+    // showresult--> this will display the result in the main text page
+    const [showresult, setShowResult]= useState(false);
+    // loading--> it will display one loading animation
+    const [loading, setLoading]=useState(false);
+    // resultData --> use to display data on webpage
+    const [resultData, setResultData]= useState("");
+
+
 
     const onSent = async(prompt)=>{
-       await run(prompt)
+        // Using this our previos response has been removed
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)  
+
+       const response = await run(input)
+       setResultData(response)
+       setLoading(false)
+       setInput("")
+       
     }
 
-    onSent("What is react js");
+    
 
     const contextValue={
+        prevPrompt,
+        setPrevPrompt,
+        setRecentPrompt,
+        onSent,
+        recentPrompt,
+        showresult,
+        loading,
+        resultData,
+        input,
+        setInput
 
     }
 
     return(
-        <ContextProvider value={contextValue}>
+        <Context.Provider value={contextValue}>
             {props.children}
-        </ContextProvider>
+        </Context.Provider>
     )
 }
+export default ContextProvider
